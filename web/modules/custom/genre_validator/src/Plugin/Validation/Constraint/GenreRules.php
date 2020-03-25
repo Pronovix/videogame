b427cdf00311182bcc09922a0bad97fbfd25d44a
András Szucsán <szucsanandras@gmail.com>
2020-03-25 11:14:48 +0100

    Add variable doc comments

--- web/modules/custom/genre_validator/src/Plugin/Validation/Constraint/GenreRules.php
+++ web/modules/custom/genre_validator/src/Plugin/Validation/Constraint/GenreRules.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Drupal\genre_validator\Plugin\Validation\Constraint;
 
@@ -15,17 +15,31 @@ use Symfony\Component\Validator\Constraint;
  *   type = "string"
  * )
  */
-
 class GenreRules extends Constraint {
 
+  /**
+   * @var string
+   */
   public string $noNumbers = 'Genres should not contain numbers';
 
+  /**
+   * @var string
+   */
   public string $noComma = 'Only one genre is allowed per line';
 
+  /**
+   * @var string
+   */
   public string $notCapitalized = '%genre should be capitalized';
 
+  /**
+   * @var string
+   */
   public string $dashBeforeAfter = 'Dashes should be preceded and followed by a letter';
 
+  /**
+   * @var string
+   */
   public string $oneDashOrSpace = 'There should be one space or dash between words';
 
 }
