8a6cfacc4c87d370e4b120d2ac57f9267209af83
András Szucsán <szucsanandras@gmail.com>
2020-03-25 10:46:59 +0100

    Further phpcs error fixes

--- web/modules/custom/genre_validator/src/Plugin/Validation/Constraint/GenreRules.php
+++ web/modules/custom/genre_validator/src/Plugin/Validation/Constraint/GenreRules.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Drupal\genre_validator\Plugin\Validation\Constraint;
 
@@ -15,19 +16,14 @@ use Symfony\Component\Validator\Constraint;
  */
 class GenreRules extends Constraint {
 
-  // The message that will be shown if the genre submitted is not a string.
   public $noNumbers = 'Genres should not contain numbers';
 
-  // The message that will be shown if there are more then one genres on a single line.
   public $noComma = 'Only one genre is allowed per line';
 
-  // The message that will be shown if the genre submitted is not capitalized.
   public $notCapitalized = '%genre should be capitalized';
 
-  // The message that will be shown if dashes are not preceded and/or followed by a letter.
   public $dashBeforeAfter = 'Dashes should be preceded and followed by a letter';
 
-  // The message that will be shown if there are more then one spaces or dashes between words.
   public $oneDashOrSpace = 'There should be one space or dash between words';
 
 }
