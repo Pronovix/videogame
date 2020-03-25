3324265362944a5b45ee5075d337f9e0012f5940
András Szucsán <szucsanandras@gmail.com>
2020-03-25 11:51:27 +0100

    Remove GenreRulesValidator inline comments

--- web/modules/custom/genre_validator/src/Plugin/Validation/Constraint/GenreRulesValidator.php
+++ web/modules/custom/genre_validator/src/Plugin/Validation/Constraint/GenreRulesValidator.php
@@ -9,7 +9,6 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 /**
  * Validates the genre_rules Constraint.
- *
  */
 class GenreRulesValidator extends ConstraintValidator {
 
@@ -19,23 +18,23 @@ class GenreRulesValidator extends ConstraintValidator {
   public function validate($genres, Constraint $constraint) {
 
     foreach ($genres as $genre) {
-      // Check if the string has any numbers
+      // Check if the string has any numbers.
       if (preg_match('/[d]/', $genre)) {
         $this->context->addViolation($constraint->noNumbers, ['%value' => $genre->value]);
       }
-      // Check if the string has any special characters
+      // Check if the string has any special characters.
       if (preg_match('/[\W]+[^-]/', $genre)) {
         $this->context->addViolation($constraint->noComma, ['%value' => $genre->value]);
       }
-      // Check if the first letter is capitalized
+      // Check if the first letter is capitalized.
       if (preg_match('/^[A-Z]/', $genre)) {
         $this->context->addViolation($constraint->notCapitalized, ['%value' => $genre->value]);
       }
-      // Check if every dash has a letter before and after it
+      // Check if every dash has a letter before and after it.
       if (preg_match('/^[A-Za]+(?:-[A-Za]+)?$/D', $genre)) {
         $this->context->addViolation($constraint->dashBeforeAfter, ['%value' => $genre->value]);
       }
-      //Check if there are more then one consecutive spaces or dashes anywhere
+      //Check if there are more then one consecutive spaces or dashes anywhere.
       if (preg_match('/^(\w+([\s-]\w+)?)+$/', $genre)) {
         $this->context->addViolation($constraint->oneDashOrSpace, ['%value' => $genre->value]);
 
